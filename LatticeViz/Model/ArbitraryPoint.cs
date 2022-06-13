92255e49352ec8f45bbda8a9f4de02724476e7d8
Bogdan Mezei <bogdan335@hotmail.com>
2022-06-13 19:11:00 +0200

    Closest Point

---
+++ LatticeViz/Model/ArbitraryPoint.cs
@@ -0,0 +1,13 @@
+﻿namespace LatticeViz.Model;
+
+public class ArbitraryPoint
+{
+    public double x { get; set; }
+    public double y { get; set; }
+
+    public ArbitraryPoint(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
--- LatticeViz/Model/Lattice.cs
+++ LatticeViz/Model/Lattice.cs
@@ -18,9 +18,9 @@ namespace LatticeViz.Model
 
         public ObservableCollection<LatticePoint> ReturnLatticePoints()
         {
-            for (int i = -60; i <= 60; i++)
+            for (int i = -30; i <= 30; i++)
             {
-                for (int j = -60; j <= 60; j++)
+                for (int j = -30; j <= 30; j++)
                 {
                     lattice.Add(new LatticePoint(j*baseX.x+i*baseY.x,j*baseX.y+i*baseY.y));
                 }
@@ -30,19 +30,35 @@ namespace LatticeViz.Model
 
         public void BasisReductionCheck()
         {
-            int k1 = (int) Math.Round(((double) baseX.x * baseY.x + baseX.y * baseY.y) / (baseX.x * baseX.x + baseX.y * baseX.y));
-            int k2 = (int) Math.Round(((double) baseX.x * baseY.x + baseX.y * baseY.y) / (baseY.x * baseY.x + baseY.y * baseY.y));
-            if (k2 != 0)
+            if (!((baseX.x == 0 && baseX.y == 0) || (baseY.x == 0 && baseY.y == 0)))
             {
-                baseX.x = baseX.x - k2 * baseY.x;
-                baseX.y = baseX.y - k2 * baseY.y;
-                BasisReductionCheck();
-            }
-            else if (k1 != 0)
-            {
-                baseY.x = baseY.x - k1 * baseX.x;
-                baseY.y = baseY.y - k1 * baseX.y;
-                BasisReductionCheck();
+                int k1 = (int)Math.Round(((double)baseX.x * baseY.x + baseX.y * baseY.y) /
+                                         (baseX.x * baseX.x + baseX.y * baseX.y),MidpointRounding.AwayFromZero);
+                int k2 = (int)Math.Round(((double)baseX.x * baseY.x + baseX.y * baseY.y) /
+                                         (baseY.x * baseY.x + baseY.y * baseY.y),MidpointRounding.ToZero);
+                Console.WriteLine("--------");
+                Console.WriteLine(k1);
+                Console.WriteLine(k2);
+                Console.WriteLine("----X----");
+                Console.WriteLine(baseX.x);
+                Console.WriteLine(baseX.y);
+                Console.WriteLine("----Y----");
+                Console.WriteLine(baseY.x);
+                Console.WriteLine(baseY.y);
+                if (k2 != 0 && Math.Abs(k2) >= Math.Abs(k1))
+                {
+                    baseX.x = baseX.x - k2 * baseY.x;
+                    baseX.y = baseX.y - k2 * baseY.y;
+                    if (!(baseX.x == 0 && baseX.y == 0))
+                        BasisReductionCheck();
+                }
+                else if (k1 != 0 && Math.Abs(k1) >= Math.Abs(k2))
+                {
+                    baseY.x = baseY.x - k1 * baseX.x;
+                    baseY.y = baseY.y - k1 * baseX.y;
+                    if (!(baseY.x == 0 && baseY.y == 0))
+                        BasisReductionCheck();
+                }
             }
         }
     }
